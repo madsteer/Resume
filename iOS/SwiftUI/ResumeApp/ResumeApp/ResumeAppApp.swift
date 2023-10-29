d132e16205cb8d1183598b2895825dfc05ba6a3f
Cory Steers <cory.steers.vaermy@statefarm.com>
2023-10-29 14:46:03 -0500

    SwiftUI ResumeApp: initical creation and Core Data Model

---
+++ iOS/SwiftUI/ResumeApp/ResumeApp/ResumeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeAppApp.swift
+//  ResumeApp
+//
+//  Created by Cory Steers on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ResumeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
