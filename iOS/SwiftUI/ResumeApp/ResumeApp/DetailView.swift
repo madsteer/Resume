6ef90dc040943272e9abd560e3eb1e088dca7a74
Cory Steers <developer@madsteer.com>
2023-10-31 18:07:39 -0500

    swiftui: wire up some initial ui scafolding

---
+++ iOS/SwiftUI/ResumeApp/ResumeApp/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ResumeApp
+//
+//  Created by Cory Steers on 10/31/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
