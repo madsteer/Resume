4a13b86a0980a5ef3b211d58e0da249d2ce62eea
Cory Steers <developer@madsteer.com>
2023-11-02 00:05:37 -0500

    swiftui: add extensions to make CoreData objects more easily usable, encapsulating optional garbage into one place

---
+++ iOS/SwiftUI/ResumeApp/ResumeApp/Tag-CoreDataHelpers.swift
@@ -0,0 +1,47 @@
+//
+//  Tag-CoreDataHelpers.swift
+//  ResumeApp
+//
+//  Created by Cory Steers on 11/1/23.
+//
+
+import Foundation
+
+extension Tag {
+    var tagID: UUID {
+        id ?? UUID()
+    }
+
+    var tagName: String {
+        name ?? ""
+    }
+
+    var tagActiveIssues: [Issue] {
+        let result = issues?.allObjects as? [Issue] ?? []
+        return result.filter { $0.completed == false }
+    }
+
+    static var example: Tag {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+
+        return tag
+    }
+}
+
+extension Tag: Comparable {
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        let left = lhs.tagName.localizedLowercase
+        let right = rhs.tagName.localizedLowercase
+
+        if left == right {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        }
+
+        return left < right
+    }
+}
