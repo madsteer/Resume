f7d21569d2f340d986bdbffb59988198b3bef4a8
Cory Steers <developer@madsteer.com>
2023-11-17 16:23:40 -0600

    SwiftUI: add simple haptic tap for closing an issue

--- iOS/SwiftUI/ResumeApp/ResumeApp/Editing/IssueViewToolbar.swift
+++ iOS/SwiftUI/ResumeApp/ResumeApp/Editing/IssueViewToolbar.swift
@@ -5,13 +5,15 @@
 //  Created by Cory Steers on 11/10/23.
 //
 
+//import CoreHaptics // Don't need for simple success
 import SwiftUI
 
 /// Provide the view for the Issue view's toolbar menu
 struct IssueViewToolbar: View {
     @EnvironmentObject var dataController: DataController
-
     @ObservedObject var issue: Issue
+
+//    @State private var engine = try? CHHapticEngine() // Don't need for simple success
 
     var openCloseButtonText: LocalizedStringKey {
         issue.completed ? "Re-open Issue" : "Close Issue"
@@ -26,11 +28,17 @@ struct IssueViewToolbar: View {
             }
 
             Button {
-                issue.completed.toggle()
-                dataController.save()
+                toggleCompleted()
             } label: {
                 Label(openCloseButtonText, systemImage: "bubble.left.and.exclamationmark.bubble.right")
             }
+//            .sensoryFeedback(trigger: issue.completed) { _, newValue in // requires iOS 17
+//                if newValue {
+//                    .success
+//                } else {
+//                    nil
+//                }
+//            }
 
             Divider()
 
@@ -39,6 +47,39 @@ struct IssueViewToolbar: View {
             }
         } label: {
             Label("Actions", systemImage: "ellipsis.circle")
+        }
+    }
+
+    func toggleCompleted() {
+        issue.completed.toggle()
+        dataController.save()
+
+        if issue.completed {
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+
+//            do { // really fancy haptic I'm not going to use
+//                try engine?.start()
+//
+//                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+//                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+//
+//                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 1)
+//                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 0)
+//
+//                let parameter1 = CHHapticParameterCurve(parameterID: .hapticIntensityControl,
+//                                                        controlPoints: [start, end], relativeTime: 0)
+//
+//                let event1 = CHHapticEvent(eventType: .hapticTransient,
+//                                           parameters: [intensity, sharpness], relativeTime: 0)
+//                let event2 = CHHapticEvent(eventType: .hapticContinuous,
+//                                           parameters: [sharpness, intensity], relativeTime: 0.125, duration: 1)
+//
+//                let pattern = try CHHapticPattern(events: [event1, event2], parameterCurves: [parameter1])
+//                let player = try engine?.makePlayer(with: pattern)
+//                try player?.start(atTime: 0)
+//            } catch {
+//                // didn't work, but that's OK
+//            }
         }
     }
 }
